380b5a9f2d5056257158ea13608a8a3fd14e57f2
AntCasson <anthonyrcasson@gmail.com>
2022-03-03 23:26:30 +0100

    added state and onChange handler

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,21 @@
 import Head from "next/head";
 import Image from "next/image";
+import { useState } from "react";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
+  const [recipe, setRecipe] = useState({
+    numberOfPizzas: 2,
+    sizeOfPizza: 125,
+    hydration: 60
+  });
+
+  const onChange = e => {
+    const name = e.target.name;
+    const value = e.target.value;
+    setRecipe(prev => ({ ...prev, [name]: value }));
+  };
+
   return (
     <div className={styles.container}>
       <Head>
@@ -15,31 +28,62 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          PizzaPlotter <code>2.0</code>
-        </h1>
+        <header className={styles.header}>
+          <h1 className={styles.title}>
+            PizzaPlotter <code>2.0</code>
+          </h1>
 
-        <p className={styles.description}>
-          Generate a great pizza recipe in{" "}
-          <span className={styles.textAccent}>4</span> steps.
-        </p>
+          <p className={styles.description}>Don't cry, it's just a pie.</p>
+        </header>
 
         <div className={styles.grid}>
           <section className={styles.card}>
             <h2>How many pizza's?</h2>
-            <p>Slider or better why to choose the size</p>
+            <input
+              onChange={onChange}
+              type='range'
+              name='numberOfPizzas'
+              value={recipe.numberOfPizzas}
+              min='1'
+              max='6'
+              className={styles.slider}
+            ></input>
+            <p>{recipe.numberOfPizzas}</p>
           </section>
 
           <section className={styles.card}>
             <h2>What size?</h2>
-            <p>checkboxes for PIES SIZE</p>
+            <input
+              onChange={onChange}
+              type='range'
+              name='sizeOfPizza'
+              value={recipe.sizeOfPizza}
+              min='100'
+              max='300'
+              className={styles.slider}
+            ></input>
           </section>
 
           <section className={styles.card}>
-            <h2>How much water?</h2>
-            <p>Slider for water </p>
+            <h2>Hydration level?</h2>
+            <input
+              onChange={onChange}
+              type='range'
+              name='hydration'
+              value={recipe.hydration}
+              min='60'
+              max='75'
+              className={styles.slider}
+            ></input>
+            {/* <p>
+              The more water you add the stickier the dough will be. Even though
+              it's hard to handle at first it definitely make your pizza's a lot
+              tastier. Specially if you are baking your pie in a home oven.
+            </p> */}
           </section>
         </div>
+
+        <div className='recipe__wrapper'></div>
       </main>
 
       <footer className={styles.footer}>
@@ -53,8 +97,8 @@ export default function Home() {
             <Image
               src='/icon-192x192.png'
               alt='PizzaPlotter Icon'
-              width={28}
-              height={28}
+              width={22}
+              height={22}
             />
           </span>
           by AntCasson
